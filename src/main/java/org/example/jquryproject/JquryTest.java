420231550bbd8c43fe9ab9d51cb6d19a9c50100a
miliguli <80015608+miliguli@users.noreply.github.com>
2021-03-17 13:07:55 -0400

    file added

--- src/main/java/org/example/jquryproject/JquryTest.java
+++ src/main/java/org/example/jquryproject/JquryTest.java
@@ -1,5 +1,4 @@
-package jquryproject;
-
+package org.example.jquryproject;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,7 +9,6 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class JquryTest {
